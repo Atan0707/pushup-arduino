05c8b1e0a3ba4297005225137ca073b55579478b
atan <harizhakim85@gmail.com>
2024-06-08 21:01:06 +0800

    added lcd

--- pushup-project.ino
+++ pushup-project.ino
@@ -1,9 +1,12 @@
 //pushup project for IoT
-
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #define echoPin 2               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
 #define trigPin 4               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
 #define buzzPin 15    
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+
+int counter = 0;
 long duration, distance;
 
 void setup() {
@@ -12,11 +15,15 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzPin, OUTPUT);
-
+  lcd.init();                       // Initialize the LCD
+  lcd.backlight();                  // Turn on the backlight
+  lcd.clear();                      // Clear the LCD screen
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lcd.setCursor(0, 0);               // Set the cursor to the first column and first row
+  lcd.print("    PUSHUP COUNTER   ");     // Print some text
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -33,10 +40,14 @@ void loop() {
   
 
   if (distance <= 15) {
+    counter++;
     digitalWrite(buzzPin, HIGH);  // Turn the buzzer on
-    delay(500);                     // Wait for 500 milliseconds
+    delay(100);                   // Wait for 100 milliseconds
     digitalWrite(buzzPin, LOW);   // Turn the buzzer off
-    delay(500);                     // Wait for 500 milliseconds
+    lcd.setCursor(0, 1);          // Move to the second row
+    lcd.print("Total = ");
+    lcd.print(counter);
+    delay(1000);                  // Delay to avoid multiple counts for one pushup
   }
   delay(10);
 }
